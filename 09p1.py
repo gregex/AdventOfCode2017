903242baa6fe958076c91315cb853288ed8be534
gregor <gregor.orlic1@gmail.com>
2017-12-09 13:41:56 +0100

    soultions for 9th day

---
+++ 09p1.py
@@ -0,0 +1,37 @@
+from fileinput import input
+
+data = input("string.txt")
+summed = 0
+layer = 0
+
+for row in data:
+    row=row.strip()
+
+i = 0
+noExc = ""
+while i<len(row):
+    if row[i]=='!':
+        i+=2
+    else:
+        noExc+=row[i]
+        i+=1
+
+i = 0
+noGarbage = ""
+while i<len(noExc):
+    if noExc[i]=="<":
+        while noExc[i]!=">":
+            i+=1
+    else:
+        noGarbage+=noExc[i]
+    i+=1
+
+i = 0
+while i<len(noGarbage):
+    if noGarbage[i]=="{":
+        layer+=1
+    elif noGarbage[i]=="}":
+        summed += layer
+        layer -= 1
+    i+=1
+print(summed)
---
+++ 09p2.py
@@ -0,0 +1,28 @@
+from fileinput import input
+
+data = input("string.txt")
+summed = 0
+layer = 0
+
+for row in data:
+    row=row.strip()
+
+i = 0
+noExc = ""
+while i<len(row):
+    if row[i]=='!':
+        i+=2
+    else:
+        noExc+=row[i]
+        i+=1
+
+i = 0
+while i<len(noExc):
+    if noExc[i]=="<":
+        i+=1
+        while noExc[i]!=">":
+            i+=1
+            summed+=1
+    i+=1
+
+print(summed)
